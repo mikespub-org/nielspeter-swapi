0a57ffa7bdbbb1a6f2cb86703df44c7d2817c370
Leonid Podriz <leonidpodriz@gmail.com>
2020-07-18 00:35:42 +0300

    Commented django heroku

--- swapi/settings.py
+++ swapi/settings.py
@@ -1,5 +1,5 @@
 import os
-import django_heroku
+# import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -175,4 +175,4 @@ INTERNAL_IPS = [
     'localhost',
 ]
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
